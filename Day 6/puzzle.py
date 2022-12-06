d28823e398dcefe707b84c19bc3f4867f28f8aa9
Kevin <104761532+Hasakev@users.noreply.github.com>
2022-12-06 21:26:28 +1000

    Day 6 complete
    Minor clean-ups to original submission

--- Day 6/puzzle.py
+++ Day 6/puzzle.py
@@ -2,8 +2,8 @@ def puzzle_solver(marker):
     file = open("Day 6\input.txt", "r")
     array = []
     line = file.read()
-    for i in range(len(line)):
-        array.append(line[i])
+    for i in line:
+        array.append(i)
         if len(array) >= marker:
             if check(array, marker):
                 file.close()
@@ -16,6 +16,5 @@ def check(array, marker):
         return True
     return False
                        
-
 print(puzzle_solver(4)) # puzzle 1 
 print(puzzle_solver(14)) # puzzle 2
